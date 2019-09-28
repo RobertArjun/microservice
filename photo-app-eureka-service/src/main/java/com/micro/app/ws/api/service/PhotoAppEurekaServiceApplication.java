7fafa0f4356b2a2363c94d9134014d9e79a7e00c
rober <rober@Robert-PC>
2019-09-28 23:55:03 +0530

    First Commit

---
+++ photo-app-eureka-service/src/main/java/com/micro/app/ws/api/service/PhotoAppEurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.micro.app.ws.api.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PhotoAppEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppEurekaServiceApplication.class, args);
+	}
+
+}
